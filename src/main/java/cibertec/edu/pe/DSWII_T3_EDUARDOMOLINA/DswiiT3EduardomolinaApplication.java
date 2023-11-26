812708b5fe34e5b45af13de24502f3a588194a34
EduardoMolinaC <clausmolina1999@gmail.com>
2023-11-25 21:30:28 -0500

    primer commit

---
+++ src/main/java/cibertec/edu/pe/DSWII_T3_EDUARDOMOLINA/DswiiT3EduardomolinaApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_EDUARDOMOLINA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3EduardomolinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3EduardomolinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DSWII_T3_EDUARDOMOLINA/DswiiT3EduardomolinaApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_EDUARDOMOLINA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3EduardomolinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
